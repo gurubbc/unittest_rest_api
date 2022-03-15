12c847b82ba06cf84684f692d1c091583c0e999b
gurubbc <gurumurthy.ramamurthy@gmail.com>
2022-03-15 15:56:47 +0530

    version 1

---
+++ unit-testing-rest/src/main/java/com/ofss/springboot/StudentServicesApplication.java
@@ -0,0 +1,13 @@
+package com.ofss.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentServicesApplication.class, args);
+		System.out.println("Main app started...");
+	}
+}
